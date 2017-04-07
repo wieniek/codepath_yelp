617f0052e4f139ddfd20eae8f5a88538805c13c9
Wieniek Sliwinski <wieniek@gmail.com>
2017-04-07 07:24:04 +0100

    Added Filters table view

--- Yelp/FiltersViewController.swift
+++ Yelp/FiltersViewController.swift
@@ -8,15 +8,28 @@
 
 import UIKit
 
-class FiltersViewController: UIViewController {
+class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
   
   @IBOutlet weak var tableView: UITableView!
   
+  var categories: [[String:String]]!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    categories = yelpCategories()
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchCell", for: indexPath) as! SwitchCell
+    cell.switchLabel.text = categories[indexPath.row]["name"]
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return categories.count
   }
   
   override func didReceiveMemoryWarning() {
@@ -31,17 +44,13 @@ class FiltersViewController: UIViewController {
     dismiss(animated: true, completion: nil)
   }
   
-  
-  
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  func yelpCategories() -> [[String:String]] {
+    return [
+      ["name" : "Soup", "code" : "soup"],
+      ["name" : "Thai", "code" : "thai"],
+      ["name" : "Seafood", "code" : "seafood"],
+      ["name" : "Irish", "code" : "irish"]
+    ]
+  }
   
 }
